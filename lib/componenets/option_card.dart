9741a37b7f85c7300da40cd8a5a3bcf896dfe2ab
shashanknegi29 <74415202+shashanknegi29@users.noreply.github.com>
2024-04-30 22:21:27 +0530

    final commit

--- lib/componenets/option_card.dart
+++ lib/componenets/option_card.dart
@@ -1,156 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:soccer_grid/componenets/already_answered.dart';
-// import 'package:soccer_grid/componenets/draw_dialog.dart';
-// import 'package:soccer_grid/componenets/winner_dialog.dart';
-// import 'package:soccer_grid/providers/player_name_provider.dart';
-// import 'package:soccer_grid/vars/globals.dart';
-// import 'package:soccer_grid/providers/points_provider.dart';
-
-// class OptionCard extends StatefulWidget {
-//   final String option;
-//   final String correctOption;
-//   final Size size;
-//   @override
-//   final Key? key;
-//   const OptionCard({
-//     required this.option,
-//     required this.correctOption,
-//     required this.size,
-//     this.key
-//   }) : super(key: key);
-
-//   @override
-//   State<OptionCard> createState() => _OptionCardState();
-// }
-                                                                           
-// class _OptionCardState extends State<OptionCard> {
-//   Color cardColor = Colors.amber; // Default color is yellow
-//   // ignore: unused_field
-//   late String _option;
-//   // ignore: unused_field
-//   late String _correctOption;
-//   @override
-//   void initState() {
-//     super.initState();              
-//     _option = widget.option;
-//     _correctOption = widget.correctOption;
-//   }
-
-//   @override
-//   void didUpdateWidget(covariant OptionCard oldWidget) {
-//     super.didUpdateWidget(oldWidget);
-//     if (oldWidget.option != widget.option || oldWidget.correctOption != widget.correctOption) {
-//       setState(() {
-//         _option = widget.option;
-//         _correctOption = widget.correctOption;
-//         setState(() {
-//           cardColor = Colors.amber;
-//         });
-//       });
-//     }
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return GestureDetector(
-//       onTap: () {
-
-//           // Shows a dialog box if the player tries to answer same question twice.
-//           if(playerturn%2==1 && playerAnswered==true) {
-//             showDialog(
-//                   context: context, 
-//                   builder: (context)=>const AlreadyAnswered()
-//                  );
-//           }
-//           else if(playerturn%2==0 && playerAnswered==true) {
-//             showDialog(
-//                   context: context, 
-//                   builder: (context)=>const AlreadyAnswered()
-//                  );
-//           }
-//           // Update the card color based on whether the selected option is correct or not
-//           else{
-            
-//             cardColor = widget.option == widget.correctOption ? Colors.green : Colors.red;  // pass this playerAnswer to the player_score using a callback function
-
-//             setState(() {
-//               if(playerturn%2==0 && widget.option == widget.correctOption) {
-//               player1correct = true;
-//               context.read<PointsProvider>().incrementPlayer1();
-//             }
-//             else if(playerturn%2==0 && (widget.option  != widget.correctOption)) {
-//               player1correct=false;
-//               context.read<PointsProvider>().decrementPlayer1();
-//             }
-//             if(playerturn%2==1 && widget.option == widget.correctOption) {
-//               player2correct = true;
-//               context.read<PointsProvider>().incrementPlayer2();
-//             }
-//             else if(playerturn%2==1 && (widget.option  != widget.correctOption)) {
-//               player2correct = false;
-//               context.read<PointsProvider>().decrementPlayer2();
-//             }
-//             // rebuild the build function of PlayerScore.
-//             if(playerturn%2==0) {
-//               playerAnswered=true;
-//             }
-//             else{
-//               playerAnswered=true;
-//             }
-//             });
-//             playerturn++;
-//             print("currently $playerturn");
-//             if (playerturn >= 16) {
-//               print('I am inside the >16 condition');
-//               playerturn = 0;
-              
-//               var pointsProvider = Provider.of<PointsProvider>(context, listen: false);
-//               if (pointsProvider.player1Score != pointsProvider.player2Score) {
-//                 showDialog(
-//                   context: context,
-//                   builder: (context) => WinnerDialog(
-//                     winnerName: pointsProvider.player1Score > pointsProvider.player2Score
-//                         ? Provider.of<NameProvider>(context, listen: false).player1Name
-//                         : Provider.of<NameProvider>(context, listen: false).player2Name,
-//                   ),
-//                 );
-//               } else {
-//                 showDialog(
-//                   context: context,
-//                   builder: (context) => const DrawDialog(),
-//                 );
-//               }
-//             }
-
-            
-//           }
-          
-
-//       },
-//       child: AnimatedContainer(
-//         duration: const Duration(milliseconds: 200), // Animation duration
-//         height: 70,
-//         width: 190,
-//         decoration: BoxDecoration(
-//           color: cardColor, // Use the updated card color
-//           borderRadius: BorderRadius.circular(10), // Rounded corners for the card
-//           border: Border.all(color: Colors.black), // Black border around the card
-//         ),
-//         child: Center(
-//           child: Text(
-//             widget.option,
-//             style: const TextStyle(
-//               color: Colors.black, // Text color is white for better visibility
-//               fontWeight: FontWeight.bold, // Text is bold
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:soccer_grid/componenets/already_answered.dart';
@@ -179,44 +26,35 @@ class OptionCard extends StatefulWidget {
 
 class _OptionCardState extends State<OptionCard> {
   Color cardColor = Colors.amber; // Default color is yellow
-  late String option;
-  late String correctOption;
-
+  // ignore: unused_field
+  late String _option;
+  // ignore: unused_field
+  late String _correctOption;
   @override
   void initState() {
     super.initState();              
-    option = widget.option;
-    correctOption = widget.correctOption;
+    _option = widget.option;
+    _correctOption = widget.correctOption;
   }
 
   @override
-  void didUpdateWidget(OptionCard oldWidget) {
+  void didUpdateWidget(covariant OptionCard oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.option != widget.option || oldWidget.correctOption != widget.correctOption) {
       setState(() {
-        option = widget.option;
-        correctOption = widget.correctOption;
-        cardColor = Colors.amber;
+        _option = widget.option;
+        _correctOption = widget.correctOption;
+        setState(() {
+          cardColor = Colors.amber;
+        });
       });
     }
   }
-
   @override
   Widget build(BuildContext context) {
     
     return GestureDetector(
       onTap: () {
-        print('$playerturn is current playerturn');
-        
-        if ((playerturn % 2 == 1 && player1Answered) || (playerturn % 2 == 0 && player2Answered)) {
-          showDialog(
-            context: context,
-            builder: (context) => const AlreadyAnswered(),
-          );
-        } else {
-          playerturn++;
-          setState(() {
-            cardColor = widget.option == widget.correctOption ? Colors.green : Colors.red;
 
           // Shows a dialog box if the player tries to answer same question twice.
           if(playerturn%2==1 && playerAnswered==true) {
@@ -283,33 +121,28 @@ class _OptionCardState extends State<OptionCard> {
                   builder: (context) => const DrawDialog(),
                 );
               }
-              player2Answered = true;
             }
 
             
           }
           
 
-            
-          });
-        }
-        
       },
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 200), // Animation duration
         height: widget.size.height,
         width: widget.size.width,
         decoration: BoxDecoration(
-          color: cardColor,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Colors.black),
+          color: cardColor, // Use the updated card color
+          borderRadius: BorderRadius.circular(10), // Rounded corners for the card
+          border: Border.all(color: Colors.black), // Black border around the card
         ),
         child: Center(
           child: Text(
             widget.option,
             style: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+              color: Colors.black, // Text color is white for better visibility
+              fontWeight: FontWeight.bold, // Text is bold
             ),
           ),
         ),
